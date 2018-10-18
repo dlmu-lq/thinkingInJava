ae0fa5af24a91570bbc56b4c48b8fac849f9a8d0
liqiang <1725055744@qq.com>
2018-10-18 22:39:57 +0800

    simple proxy

--- src/main/java/top/itlq/thinkingInJava/typeinfo_14/proxy/SimpleProxy.java
+++ src/main/java/top/itlq/thinkingInJava/typeinfo_14/proxy/SimpleProxy.java
@@ -3,18 +3,30 @@ package top.itlq.thinkingInJava.typeinfo_14.proxy;
 public class SimpleProxy implements Interface{
     private Interface proxied;
 
-    public SimpleProxy(){
-
+    public SimpleProxy(Interface proxied){
+        this.proxied = proxied;
     }
 
     @Override
     public void doSomething() {
-
+        System.out.println("proxy doSomething");
+        proxied.doSomething();
     }
 
     @Override
     public void doSomethingElse() {
+        System.out.println("proxy doSomethingElse");
+        proxied.doSomethingElse();
+    }
 
+    static void consumer(Interface obj){
+        obj.doSomething();
+        obj.doSomethingElse();
+    }
+
+    public static void main(String...args){
+        SimpleProxy simpleProxy = new SimpleProxy(new RealObject());
+        consumer(simpleProxy);
     }
 }
 
