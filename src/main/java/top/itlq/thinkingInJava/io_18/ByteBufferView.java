c09175b5df7ac125907f9100e29cdf1dd9c22356
liqiang <1725055744@qq.com>
2018-11-05 22:53:59 +0800

    nio:2

--- src/main/java/top/itlq/thinkingInJava/io_18/ByteBufferView.java
+++ src/main/java/top/itlq/thinkingInJava/io_18/ByteBufferView.java
@@ -5,14 +5,25 @@ package top.itlq.thinkingInJava.io_18;
 
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
+
 public class ByteBufferView {
     /**
      * 对视图的操作都反映在实际缓冲器中
      * 注意put方法
      */
     @Test
-    public void test(){
-
+    public void test1(){
+        ByteBuffer buff = ByteBuffer.allocate(1024);
+        IntBuffer intBuffer = buff.asIntBuffer();
+        intBuffer.put(65);
+        buff.flip(); // 测试，无用
+        buff.clear(); // 测试，无用
+//        buff.reset(); // position = mark 没有mark抛异常
+        System.out.println(buff.getChar());
+        System.out.println(buff.getChar());
     }
 
     /**
@@ -20,4 +31,27 @@ public class ByteBufferView {
      * hasRemaining() position() get();
      * double
      */
+    @Test
+    public void test2(){
+        ByteBuffer buff = ByteBuffer.wrap(new byte[]{0,0,0,65});
+        while (buff.hasRemaining())
+            System.out.println("char：" + buff.getChar());
+        buff.rewind();
+        ShortBuffer shortBuffer = buff.asShortBuffer();
+        while (shortBuffer.hasRemaining())
+            System.out.println("short：" + shortBuffer.get());
+        buff.rewind();
+        IntBuffer intBuffer = buff.asIntBuffer();
+        while (intBuffer.hasRemaining())
+            System.out.println("int：" + intBuffer.get());
+    }
+
+    /**
+     * 利用ByteBuffer 使字符串倒序（任意调换顺序）
+     * todo
+     */
+    @Test
+    public void test3(){
+
+    }
 }
