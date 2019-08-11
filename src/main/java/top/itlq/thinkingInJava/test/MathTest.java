11396a5cc916874ca24537b91f10bb5b5650ac6e
liqiang <1725055744@qq.com>
2019-08-11 23:15:24 +0800

    nio review.

---
+++ src/main/java/top/itlq/thinkingInJava/test/MathTest.java
@@ -0,0 +1,13 @@
+package top.itlq.thinkingInJava.test;
+
+import java.math.BigInteger;
+
+public class MathTest {
+    public static void main(String...args){
+        System.out.println(Math.round(-1.5)); // -1,mysql中结果为-2;
+        System.out.println(-7 % 4);
+        System.out.println(new BigInteger("-7").remainder(new BigInteger("4")));
+        System.out.println(new BigInteger("-7").mod(new BigInteger("4")));
+        System.out.println(new BigInteger("-5").mod(new BigInteger("3")));
+    }
+}
